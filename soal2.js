2dae1d3ae630350e402e95b4d7bc4f16d68c4070
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-01-24 22:49:45 +0700

    revisi tugas javascript2

--- soal2.js
+++ soal2.js
@@ -1,23 +1,40 @@
 const name = [
-    "Abigail", "Alexandra", "Alison",
-    "Amanda", "Angela", "Bella",
-    "Carol", "Caroline", "Carolyn",
-    "Deirdre", "Diana", "Elizabeth",
-    "Ella", "Faith", "Olivia", "Penelope"
-]
+        "Abigail", "Alexandra", "Alison",
+        "Amanda", "Angela", "Bella",
+        "Carol", "Caroline", "Carolyn",
+        "Deirdre", "Diana", "Elizabeth",
+        "Ella", "Faith", "Olivia", "Penelope"
+    ] // inisialisai variabel name yang dengan data array nama
 
-function searchName(hurufDicari, jumlahOutput, callback) {
+function searchName(hurufDicari, jumlahOutput, callback) { // insialisai function dengan parameter hurufDicari, jumlahOutput dan callback 
+    /* paramter hurufDicari : menampung huruf . kata yang akan dicari
+       paramter jumlahOutput : menampung jimlah maksimal data yang akan ditampilkan
+       paramter callback : untuk meampung fuunction. */
+
+    // proses memfilter data array yang akan dicari berdasarkan parameter hurufDicari
     var filterName = name.filter(function(data) {
+        /* menggunakan method filter yang didalamnya ada fungsi dengan parameter data.
+        parameter data ini yang nantinya akan mennampilkan nilai di dalam array */
         return data.toLowerCase().includes(hurufDicari)
+            /* mengembalikan nilai berdasarkan huruf yang dicari dengan cara parameter data dibuat menjadi huruf kecil semua 
+            dengan menggunakan method toLowerCase() dan icludes. Method toLowerCase() untuk membuat semua nama menjadi huruf kecil,
+            dan method icludes untuk mencari nama berdasarkan parameter hurufDicari*/
     })
     callback(jumlahOutput, filterName)
+        /* memanggil parameter callback dengan parameter diisi variabel jumlahOutput dan filterName.
+        variabel jumlahOutput berisi parameter dari jumlahOutput dan variabel filterName berisi data array yang telah di filter */
 }
 
 
-function printOutput(jumlahOutput, dataArray) {
-    var hasil = dataArray.filter(function(data, index) {
-        return index < jumlahOutput
+function printOutput(jumlahOutput, dataArray) { // inisialisai fungsi dengan parameter jumlahOutput dan dataArray.
+    /*
+       paramter jumlahOutput : menampung jimlah maksimal data yang akan ditampilkan
+       paramter dataArray : untuk menampung data berupa array */
+    var hasil = dataArray.filter(function(data, index) { //memfilter dataArray menggunkana method filter yang didalamnya terdapat fungsi dengan parameter data dan index.
+        /*Parameter data : menampung nilai yang ada di dalam array 
+        parameter index : menampung index dari array*/
+        return index < jumlahOutput // mengembalikan data array dengan index lebih kecil dari parameter jumlahOutput.
     })
-    console.log(hasil)
+    console.log(hasil) // output : [Alexandra,Amanda,Angela]
 }
-searchName("an", 3, printOutput)
+searchName("an", 3, printOutput) // output : [Alexandra,Amanda,Angela]
--- soal3.js
+++ soal3.js
@@ -1,19 +1,29 @@
-function SeleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
-    var hasil = []
-    if (nilaiAwal > nilaiAkhir) {
+function SeleksiNilai(nilaiAwal, nilaiAkhir, dataArray) { // inisialisai fungsi dengan parameter nilaiAwal, nilaiAkhir dan dataArray
+    /* paramter nilaiAwal : menampung nilai yang menjadi batas awal untuk menampilkan nilai di dalam array. 
+       paramter jumlahOutput : menampung nilai yang menjadi batas akhir untuk menampilkan nilai di dalam array. 
+       paramter dataArray : menampung data berupa array */
+    var hasil = [] // inisialisai variabel hasil dengan array kosong
+    if (nilaiAwal > nilaiAkhir) { // cek apakah niai pada parameter nilaiAwal lebih besar dari nilai pada parameter nilaiAkhir
         console.log("Nilai akhir harus lebih besar dari nilai awal")
-    } else if (dataArray.length <= 5) {
+    } else if (dataArray.length <= 5) { // jika kondisi salah, maka akan dicek apakah jumlah dataArray lebih kecil dari atau sama dengan 5
         console.log("Jumlah angka dalam dataArray harus lebih dari 5")
-    } else {
-        hasil = dataArray.filter(function(num) {
+    } else { // jika kondisi salah
+        // proses filter nilai
+        hasil = dataArray.filter(function(num) { // menggunakan method filter yang didalamnya terdapat fungsi den parameter num
             return num > nilaiAwal && num < nilaiAkhir
+                // akan mengembalikan data lebih besar dari parameter nilaiAwal dan data lebih kecil dari parameter nilaiAkhir
         })
-        hasil.sort(function(a, b) { return a - b });
-        console.log(hasil)
+        hasil.sort(function(a, b) { //setelah data difilter, maka akan diurutkan dengan menggunakan method sort yang dalamnya terdapat fungsi dengan parameter a dan b  
+            return a - b // mengembalikan nilai hasil pengurangan dari a-b 
+        });
+        /* jika hasilnya kurang dari 0, maka parameter a yang diurutkan terlebih dahulu. 
+        Jika hasilnya lebih dari 0, maka paramter b yang akan diurutkan terlebih dahulu. 
+        Jika hasilnya 0, tidak ada yang akan diurutkan */
+        console.log(hasil) // output parameter hasil
     }
 }
 
 
-SeleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8])
-SeleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8])
-SeleksiNilai(4, 17, [2, 25, 4])
+SeleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]) // output : [8,14,17]
+SeleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8]) // output : Nilai akhir harus lebih besar dari nilai awal
+SeleksiNilai(4, 17, [2, 25, 4]) // Jumlah angka dalam dataArray harus lebih dari 5s
